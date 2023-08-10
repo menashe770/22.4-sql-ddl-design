4f168e475d874e44f4e2ab14fe3bdec386fb99ff
Menashe <mborukhov770@gmail.com>
2023-08-10 15:56:22 -0600

    craigslist schema

---
+++ part 1 - schema design/craigslist.sql
@@ -0,0 +1,11 @@
+regions
+region_id | name
+
+users
+user_id | first_name | last_name | preferred_region_id
+
+posts
+post_id | title | text | user_id | location | region_id | category_id
+
+categories
+category_id | name
