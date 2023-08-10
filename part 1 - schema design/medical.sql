4c1f3fade6fba8814da439088806b288ccb82b88
Menashe <mborukhov770@gmail.com>
2023-08-10 15:24:30 -0600

    medical schema

---
+++ part 1 - schema design/medical.sql
@@ -0,0 +1,14 @@
+-- doctors
+-- doctor_id | first_name | last_name
+
+-- patients
+-- patient_id | first_name | last_name
+
+-- diseases
+-- desease_id | disease_name
+
+-- visits
+-- visit_id | visit_date | patient_id | doctor_id
+
+-- diagnoses
+-- diagnosis_id | visit_id | disease_id
