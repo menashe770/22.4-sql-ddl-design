bae07c81779a5c886256c347db06b1a3c6b07fe1
Menashe <mborukhov770@gmail.com>
2023-08-10 16:20:40 -0600

    soccer schema

---
+++ part 1 - schema design/soccer.sql
@@ -0,0 +1,17 @@
+teams
+team_id | name | rankings | season_id | home_location
+
+players
+player_id | first_name | last_name | team_id
+
+games
+game_id | date | home_team_id | away_team_id | referee_id
+
+referees
+referee_id | first_name | last_name
+
+goals
+goal_id | game_id | player_id
+
+seasons
+season_id | start_date | end_date
